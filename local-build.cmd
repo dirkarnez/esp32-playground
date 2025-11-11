0bc6c3f8b6ca0b3598c2a9c2ceda04bddd9b8991
dirkarnez <smalldirkalex@gmail.com>
2025-11-11 23:24:40 +0800

    - wip, can compile

--- local-build.cmd
+++ local-build.cmd
@@ -6,11 +6,18 @@ set DOWNLOADS_DIR_LINUX=%DOWNLOADS_DIR:\=/%
 
 set PYTHON_DIR=%DOWNLOADS_DIR%\python-3.10.8-amd64-portable
 
+set IDF_PATH=^
+%DOWNLOADS_DIR%\esp-idf;
+
+set IDF_TOOLS_PATH=%USERPROFILE%\.espressif
+set IDF_PYTHON_ENV_PATH=%IDF_TOOLS_PATH%\python_env
+
 set PATH=^
 %PYTHON_DIR%;^
 %PYTHON_DIR%\Scripts;^
 %DOWNLOADS_DIR%\PortableGit\bin;^
 %DOWNLOADS_DIR%\esp-idf-tools-portable-v1.2\tools\bin;^
+%IDF_PATH%\tools;^
 %DOWNLOADS_DIR%\cmake-3.29.3-windows-x86_64\cmake-3.29.3-windows-x86_64\bin;
 
 cmake.exe -G"Ninja" ^
--- setup.cmd
+++ setup.cmd
@@ -1,1 +1,3 @@
 https://github.com/dirkarnez/esp-idf-tools-portable/releases/download/v1.2/esp-idf-tools-portable-v1.2.zip
+
+git clone -b v5.5.1 --recursive https://github.com/espressif/esp-idf.git
