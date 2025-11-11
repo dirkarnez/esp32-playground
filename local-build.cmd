2879c958b196dcb9da64697e1a6351f54ae6e0e4
Dirk Arnez <smalldirkalex@gmail.com>
2025-11-11 22:27:39 +0800

    - update local-build.cmd

--- local-build.cmd
+++ local-build.cmd
@@ -1,4 +1,21 @@
+@REM run as Administrator
 @echo off
+cd /d %~dp0
+set DOWNLOADS_DIR=%USERPROFILE%\Downloads
+set DOWNLOADS_DIR_LINUX=%DOWNLOADS_DIR:\=/%
+
+set PYTHON_DIR=%DOWNLOADS_DIR%\python-3.10.8-amd64-portable
 
 set PATH=^
-esp-idf-tools-portable-v1.2
+%PYTHON_DIR%;^
+%PYTHON_DIR%\Scripts;^
+%DOWNLOADS_DIR%\PortableGit\bin;^
+%DOWNLOADS_DIR%\esp-idf-tools-portable-v1.2\tools\bin;^
+%DOWNLOADS_DIR%\cmake-3.29.3-windows-x86_64\cmake-3.29.3-windows-x86_64\bin;
+
+cmake.exe -G"Ninja" ^
+-DCMAKE_BUILD_TYPE=Debug ^
+-B./cmake-build &&^
+cd cmake-build &&^
+cmake --build . &&^
+echo Successful build
